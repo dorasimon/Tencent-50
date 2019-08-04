8a7e4df561e4852e81575bbf4a3faa2b9cf651f7
Kailin Xie <dorasimon@Kailins-MacBook-Air.local>
2019-08-04 20:04:27 +0800

    155 Min Stack

--- Min Stack.py
+++ Min Stack.py
@@ -1,3 +1,17 @@
+"""
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/min-stack
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
 class Minstack:
 
 	def __init__(self):
