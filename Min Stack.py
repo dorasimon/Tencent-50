1605d4db00864af108d747e977e6c7c0ac4cb8b0
Kailin Xie <dorasimon@Kailins-MacBook-Air.local>
2019-08-05 14:19:38 +0800

    Add comments

--- Min Stack.py
+++ Min Stack.py
@@ -15,21 +15,28 @@ getMin() -- Retrieve the minimum element in the stack.
 class Minstack:
 
 	def __init__(self):
-		self.items = [] #The current stack#
-		self.min_items = [] #The min stack#
+		self.items = []
+		#Create the current stack.#
+		self.min_items = []
+		#Create the min stack.#
 
 	def push(self, x):
-		self.items.append(x) #Add element x on top of the current stack."
+		self.items.append(x)
+		#Add element x on top of the current stack."
 		if len(self.min_items) == 0 or x <= self.min_items[-1]:
-			self.min_items.append(x) #Add x on top of the min stack as well when it's empty or when x is smaller than or equal to its last element.#
+			self.min_items.append(x)
+		#Add x on top of the min stack as well when it's empty or when x is smaller than or equal to its last element.#
 
 	def pop(self):
 		if self.top() == self.getMin():
 			self.min_items.pop()
-		return self.items.pop() #Always remove the last element from the current stack. If it equals the last element on the min stack, remove it from the min stack as well.#
+		return self.items.pop()
+		#Always remove the last element from the current stack. If it equals the last element on the min stack, remove it from the min stack as well.#
 
 	def top(self):
-		return self.items[-1] #Return the last element on stack.#
+		return self.items[-1]
+		#Return the last element on the current stack.#
 
 	def getMin(self):
-		return self.min_items[-1] #Return the last element on the min stack.#
+		return self.min_items[-1]
+		#Return the last element on the min stack.#
--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -1,11 +1,28 @@
+"""
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/valid-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
 class Solution:
 
 	def isValid(self, s: str):
 		if len(s) % 2 != 0:
-			return False #If s has an odd number of elements, it must be false.#
+			return False 
+		#If s has an odd number of elements, it must be false.#
 		else:
 			pairs = ["()", "[]", "{}"]
 			while any(x in s for x in pairs):
 				for pair in pairs:
 					s = s.replace(pair, "")
+		#Remove every paired bracket from s and see if there's anything left.#
 			return s == ""
