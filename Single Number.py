bf0e7cc29b97320ca0337fb09fdeadd6c88bfb0e
Kailin Xie <bead8@hotmail.com>
2019-08-12 18:17:37 +0800

    136

--- Single Number.py
+++ Single Number.py
@@ -11,9 +11,19 @@ Your algorithm should have a linear runtime complexity. Could you implement it w
 """
 
 
-class Solution:
+class Solution1:
 
 	def singleNumber(self, nums: List[int]) -> int:
 		while nums[0] in nums[1:]:
 			nums = list(filter(lambda x: x != nums[0], nums))
+			#Compare the first element with the rest of the list. If there is another one, remove them from the list and start over again.
 		return nums[0]
+
+
+class Solution2:
+
+	def singleNumber(self, nums: List[int]) -> int:
+		i = 0
+		for num in nums:
+			i ^= num
+		return i
