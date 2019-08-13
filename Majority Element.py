0589f8a1b42b6391108ce2a2e473dc7b161773cf
Kailin Xie <bead8@hotmail.com>
2019-08-13 21:26:06 +0800

    169

---
+++ Majority Element.py
@@ -0,0 +1,16 @@
+"""
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/majority-element
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution:
+
+	def majorityElement(self, nums: List[int]) -> int:
+		nums.sort()
+		return nums[len(nums) // 2]
