6f153ba30b0857eae5d8c83e1f7e6335e7ba0a36
Kailin Xie <bead8@hotmail.com>
2019-08-20 16:37:14 +0800

    Added description

--- Kth Smallest Element in a BST.py
+++ Kth Smallest Element in a BST.py
@@ -1,10 +1,22 @@
+"""
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
 class Solution:
 
 	def __init__(self):
 		self.counter = 0
 		self.result = 0
 
-	def kthSmallestElement(self, root: 'TreeNode', k: int) -> int:
+	def kthSmallestElement(self, root: TreeNode, k: int) -> int:
 		if root.left:
 			kthSmallestElement(root.left, k)
 		#Recurse until the leftmost element. First check each node's left child.
