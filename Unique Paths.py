1c3fcc2da00d49cfa350e0d8fbe5ecb9388f59d7
Kailin Xie <bead8@hotmail.com>
2019-09-16 01:01:35 +0800

    62

---
+++ Unique Paths.py
@@ -0,0 +1,17 @@
+"""
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/unique-paths
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution:
+
+	def uniquePaths(self, m: int, n: int) -> int:
+		return int(math.factorial(m + n - 2) / math.factorial(m - 1) / math.factorial(n - 1))
